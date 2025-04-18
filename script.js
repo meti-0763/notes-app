406414a214567f6a70a80fb5ffe2d1e4e3b1ade4
meti-0763 <daramade4@gmail.com>
2025-04-18 08:47:54 +0330

    enddd

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // const months = ["January", "February", "March", "April", "May", "June", "July",
 //     "August", "September", "October", "November", "December"];
-
+let time = new Date()
 let ind = 0
+let isadd = true
+
+timethis = `${time.getFullYear()} ${time.getMonth()} ${time.getDate()}`
+
+console.log(timethis);
+
 
 if (!localStorage.getItem("notes")){
     localStorage.setItem("notes",JSON.stringify([]))
@@ -17,14 +23,12 @@ function setlocal(input_arry){
 }
 function elG (){
 
-    console.log("elG");
-    document.querySelectorAll(".note").forEach(noteElm => noteElm.remove())
-    
+  
   let listnotes = getnotelocal()
-
+  
+  document.querySelectorAll(".note").forEach(noteElm => noteElm.remove())
   listnotes.forEach((element , index )=> {
      
-// console.log(index);
 
       
   let noteELEM = `      <li class="note">
@@ -63,17 +67,9 @@ function show_popup (){
 
 function closePOP (){
     document.querySelector(".popup-box").classList.remove("show")
+    document.querySelector("#inpt").value  =``
+document.querySelector("#inpd").value = ``
 }
-function addnt (){
-  let listnotes = getnotelocal()
-    let inpt = {title : document.querySelector("#inpt").value, des:document.querySelector("#inpd").value , time : "time"} 
-    listnotes.push(inpt)
-    setlocal(listnotes)
-    elG()  
-    closePOP()
-
-}
-document.querySelector("#p-b").addEventListener("click", addnt)
 document.querySelector(".uil-times").addEventListener("click", closePOP)
 document.addEventListener("click", (event) => {
   
@@ -81,57 +77,6 @@ document.addEventListener("click", (event) => {
     document.querySelectorAll(".settings").forEach(elm=>elm.classList.remove("show"))
   }
 });
-
-// function editedd (){
-//   let listnotes = getnotelocal()    
-//   document.querySelector("#p-b").addEventListener("click", (e) => {
-//     listnotes[ind].title = document.querySelector("#inpt").value
-//     listnotes[ind].des = document.querySelector("#inpd").value
-//     setlocal(listnotes)
-//     elG()
-//     document.querySelector(".popup-box").classList.remove("show")
-//     console.log("FIRD");
-    
-//     })
-// }
-
-function btnrm(th){
-  document.querySelectorAll(".settings").forEach(elm=>elm.classList.remove("show"))
-  th.parentElement.classList.add("show")
-  editedd()
-  console.log("btnrm");
-  
-  let listnotes = getnotelocal()    
-
-  document.querySelector("#p-b").addEventListener("click", (e) => {
-    listnotes[ind].title = document.querySelector("#inpt").value
-    listnotes[ind].des = document.querySelector("#inpd").value
-    setlocal(listnotes)
-    elG()
-        console.log("FIRD");
-
-    document.querySelector(".popup-box").classList.remove("show")    
-    }) 
-}
-
-document.querySelector(".add-box").addEventListener("click", ()=>{
-        document.querySelector("#inpt").value =""
-    document.querySelector("#inpd").value =""
-  document.querySelector("#p-b").addEventListener("click", ()=>{
-    let listnotes = getnotelocal()
-    let inpt = {title : document.querySelector("#inpt").value, des:document.querySelector("#inpd").value , time : "time"} 
-    listnotes.push(inpt)
-    setlocal(listnotes)
-    elG()  
-    closePOP()
-  })
-  show_popup()
-
-}
-
-
-)
-
 function delitem (index){
   
 let listnotes = getnotelocal()
@@ -141,16 +86,67 @@ listnotes.splice(index, 1)
 setlocal(listnotes)
 elG()
   }
+function addnt (){
+  let listnotes = getnotelocal()
+    let inpt = {title : document.querySelector("#inpt").value, des:document.querySelector("#inpd").value , time : timethis} 
+    listnotes.push(inpt)
+    setlocal(listnotes)
+    elG()  
+    closePOP()
+
+}
+
+function editedd (){
+  let listnotes = getnotelocal()    
+  
+    listnotes[ind].title = document.querySelector("#inpt").value
+    listnotes[ind].des = document.querySelector("#inpd").value
+    setlocal(listnotes)
+    elG()
+    document.querySelector(".popup-box").classList.remove("show")
+
+
+}
+
+function btnrm(th){
+  document.querySelectorAll(".settings").forEach(elm=>elm.classList.remove("show"))
+  th.parentElement.classList.add("show")
+  isadd = false
+}
+
+document.querySelector(".add-box").addEventListener("click", ()=>{
+  isadd = true
+    show_popup()
+        document.querySelector("#inpt").value  =``
+document.querySelector("#inpd").value = ``
+  }
+  
+  
+)
+
 
 
   function edititem (index){
-    console.log("updatyed");
-
-    ind = index
+    console.log('EDITED');
     
+    ind = index
+    let listnotes = getnotelocal()
+    document.querySelector("#inpt").value = listnotes[index].title
+    document.querySelector("#inpd").value = listnotes[index].des
     show_popup()    
 }
 
 
+
+
+document.querySelector("#p-b").addEventListener("click", (e) => {
+
+  if(isadd){
+    addnt ()
+  }else{
+   
+    editedd ()
+  } 
+  }) 
 
 elG()
